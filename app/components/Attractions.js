083e2de9deeddd434b496dfdf139d909b6e0796b
MYAMADDY <myamaddy.k67@rsu.ac.th>
2025-09-22 22:38:37 +0700

    Updated title in Attractions.js

--- app/components/Attractions.js
+++ app/components/Attractions.js
@@ -48,8 +48,9 @@ export default function Attractions() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">
-        Popular Tourist Attractions
+       MYA MADDY KO KO
       </h1>
+      <p>6708204</p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {attractions.map((attraction) => (
